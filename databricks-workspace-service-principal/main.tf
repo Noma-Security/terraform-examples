fd60396c1c0fd2638b7f932f14f22603f40d88ec
Roy Lazar <roy@nomasec.com>
2024-06-27 17:47:40 +0300

    support permissions for all jobs, pipelines & clusters; and permissions for single registered models, serving endpoints, connections, external locations and unity catalog registered models

--- databricks-workspace-service-principal/main.tf
+++ databricks-workspace-service-principal/main.tf
@@ -24,21 +24,165 @@ resource "databricks_permission_assignment" "add_user" {
   permissions  = ["USER"]
 }
 
-
 resource "databricks_permissions" "root_permission" {
   provider = databricks.workspace
-  # workspace_file_path = "/"
   directory_path = "/"
-  # workspace_file_id = data.databricks_directory.root.object_id
   
-  # directory_path = "/"
   access_control {
-    
-    # service_principal_name = databricks_service_principal.nomasec_sa.display_name
     service_principal_name = databricks_service_principal.nomasec_sa.application_id
-    # user_name = var.databricks_service_account_name
     permission_level = "CAN_READ"
   }
   
   depends_on = [ databricks_permission_assignment.add_user ]
 }
+
+# ========= Jobs ==========
+
+data "databricks_jobs" "this" {}
+
+resource "databricks_permissions" "view_all_jobs" {
+  for_each = data.databricks_jobs.this.ids
+  job_id   = each.value
+
+  access_control {
+    service_principal_name = databricks_service_principal.nomasec_sa.application_id
+    permission_level       = "CAN_VIEW"
+  }
+}
+
+# ========= Clusters ==========
+
+data "databricks_clusters" "this" {}
+
+resource "databricks_permissions" "view_all_clusters" {
+  for_each = toset(data.databricks_clusters.this.ids)
+  cluster_id = each.value
+
+  access_control {
+    service_principal_name = databricks_service_principal.nomasec_sa.application_id
+    permission_level       = "CAN_ATTACH_TO"
+  }
+}
+
+# ========= Pipelines ==========
+
+data "databricks_pipelines" "this" {}
+
+resource "databricks_permissions" "view_all_pipelines" {
+  for_each = toset(data.databricks_pipelines.this.ids)
+  pipeline_id = each.value
+
+  access_control {
+    service_principal_name = databricks_service_principal.nomasec_sa.application_id
+    permission_level       = "CAN_VIEW"
+  }
+}
+
+# ========= Registered Models ==============
+#
+# resource "databricks_mlflow_model" "this" {
+#   name = "My MLflow Model"
+#
+#   description = "My MLflow model description"
+#
+#   tags {
+#     key   = "key1"
+#     value = "value1"
+#   }
+#   tags {
+#     key   = "key2"
+#     value = "value2"
+#   }
+# }
+#
+# resource "databricks_permissions" "view_all_mlflow_models" {
+#   registered_model_id = databricks_mlflow_model.this.registered_model_id
+#
+#   access_control {
+#     service_principal_name = databricks_service_principal.nomasec_sa.application_id
+#     permission_level       = "CAN_READ"
+#   }
+# }
+
+
+# ========= Serving Endpoints ==============
+#
+# resource "databricks_model_serving" "this" {
+#   name = "ads-serving-endpoint-3"
+#   config {
+#     served_entities {
+#       name                  = "prod_model"
+#       entity_name           = "test2"
+#       entity_version        = "1"
+#       workload_size         = "Small"
+#       scale_to_zero_enabled = true
+#     }
+#   }
+# }
+#
+# resource "databricks_permissions" "ml_serving_endpoint_usage" {
+#   serving_endpoint_id = databricks_model_serving.this.serving_endpoint_id
+#
+#   access_control {
+#     service_principal_name = databricks_service_principal.nomasec_sa.application_id
+#     permission_level = "CAN_VIEW"
+#   }
+# }
+
+# ============== Connections ===============
+#
+# resource "databricks_connection" "mysql" {
+#   name            = "mysql_connection"
+#   connection_type = "MYSQL"
+#   comment         = "this is a connection to mysql db"
+#   options = {
+#     host     = "test.mysql.database.azure.com"
+#     port     = "3306"
+#     user     = "user"
+#     password = "password"
+#   }
+#   properties = {
+#     purpose = "testing"
+#   }
+# }
+#
+# resource "databricks_grants" "some" {
+#   foreign_connection = databricks_connection.mysql.name
+#   grant {
+#     principal  = databricks_service_principal.nomasec_sa.application_id
+#     privileges = ["USE_CONNECTION"]
+#   }
+# }
+
+# ============= External Locations ============
+#
+# resource "databricks_external_location" "some" {
+#   name            = "external"
+#   url             = "s3://${aws_s3_bucket.external.id}/some"
+#   credential_name = databricks_storage_credential.external.id
+#   comment         = "Managed by TF"
+# }
+#
+# resource "databricks_grants" "some" {
+#   external_location = databricks_external_location.some.id
+#   grant {
+#     principal  = databricks_service_principal.nomasec_sa.application_id
+#     privileges = ["READ_FILES"]
+#   }
+# }
+
+# ====== Unity Catalog - Registered Model =======
+#
+# resource "databricks_registered_model" "this" {
+#   name         = "my_model"
+#   catalog_name = "main"
+#   schema_name  = "default"
+# }
+#
+# resource "databricks_grants" "customers" {
+#   model = databricks_registered_model.full_name
+#   grant {
+#     principal  = databricks_service_principal.nomasec_sa.application_id
+#     privileges = ["APPLY_TAG", "EXECUTE"]
+#   }
+# }
